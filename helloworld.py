c65368bc51efb15b92fde9e39e70f6207f047a06
Ilias Stamatis <stamatis.iliass@gmail.com>
2012-01-23 14:38:16 +0200

    add from future imports for python3 compatibility

--- helloworld.py
+++ helloworld.py
@@ -2,5 +2,8 @@
 # -*- coding: utf-8 -*-
 #Visit the forum of Ubuntu-gr community => http://forum.ubuntu-gr.org/
 
+from __future__ import unicode_literals
+from __future__ import print_function
+
 print("Γεια σου κόσμε!")
 print("Hello world!")
